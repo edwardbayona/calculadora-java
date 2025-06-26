18d5ff4c17c6608153f47881a9ee2631ae288fa1
Edward Bayona <edward.bayona@gmail.com>
2025-06-25 21:31:46 -0500

    Actualizo la clase listener de calculadora con operaciones

--- NumeroListener.java
+++ NumeroListener.java
@@ -15,9 +15,67 @@ private JTextField pantalla;
 		this.pantalla = pantalla;
 	}
 	
+		
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		pantalla.setText(pantalla.getText() + texto);
+		
+		String expresion = pantalla.getText();
+		int pos;
+		
+		if(texto.equals("=")) {
+			if(expresion.indexOf("+") != -1) {
+				pos = expresion.indexOf("+");
+				determinarNumeros(expresion,pos,"+");
+			}else if(expresion.indexOf("-") != -1) {
+				pos = expresion.indexOf("-");
+				determinarNumeros(expresion,pos,"-");
+			}else if(expresion.indexOf("*") != -1) {
+				pos = expresion.indexOf("*");
+				determinarNumeros(expresion,pos,"*");
+			}else if (expresion.indexOf("/") != -1) {
+				pos = expresion.indexOf("/");
+				determinarNumeros(expresion,pos,"/");
+			}else {
+				
+			}
+		}else {
+			pantalla.setText(pantalla.getText() + texto);
+		}
+		
+		
 		//System.out.println("Boton presionado " + texto);
 	}
+	
+	public void determinarNumeros(String expresion,  int pos , String operador) {
+		String numeroUnoString;
+		String numeroDosString;
+		
+		numeroUnoString=  expresion.substring(0, pos);
+		numeroDosString= expresion.substring(pos + 1);
+		
+		double num1=Double.parseDouble(numeroUnoString);
+		double num2=Double.parseDouble(numeroDosString);
+		double resultado =0;
+		
+		switch(operador){
+			
+			case "+" : resultado=num1+num2;
+			break;
+			case "-": resultado = num1-num2;
+			break;
+			case "*": resultado = num1* num2;
+			break;
+			case "/": if(num2 != 0) {
+				resultado = num1/num2;
+			}else {
+				pantalla.setText("Error: division por 0");
+				return;
+			}
+			
+			
+		}
+		
+		pantalla.setText(String.valueOf(resultado));
+	}
+
 }
